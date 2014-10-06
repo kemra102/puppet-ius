6d260274657d8e2c9718e10002e86790eee9592a
Danny Roberts <danny.roberts@reconnix.com>
2014-10-06 15:59:45 +0100

    Missing Mirrorlists

--- manifests/init.pp
+++ manifests/init.pp
@@ -93,6 +93,7 @@ class ius (
   if $::osfamily == 'RedHat' and $::operatingsystem =~ /RedHat|CentOS/ {
     yumrepo { 'ius':
       baseurl 		=> $ius_baseurl,
+      mirrorlist	=> $ius_mirrorlist,
       failovermethod 	=> $ius_failovermethod,
       proxy 		=> $ius_proxy,
       enabled 		=> $ius_enabled,
@@ -103,6 +104,7 @@ class ius (
 
     yumrepo { 'ius-debuginfo':
       baseurl             => $ius_debuginfo_baseurl,
+      mirrorlist	  => $ius_debuginfo_mirrorlist,
       failovermethod      => $ius_debuginfo_failovermethod,
       proxy               => $ius_debuginfo_proxy,
       enabled             => $ius_debuginfo_enabled,
@@ -112,17 +114,19 @@ class ius (
     }
 
     yumrepo { 'ius-source':
-      baseurl             => $ius_baseurl,
-      failovermethod      => $ius_failovermethod,
-      proxy               => $ius_proxy,
-      enabled             => $ius_enabled,
-      gpgcheck            => $ius_gpgcheck,
+      baseurl             => $ius_source_baseurl,
+      mirrorlist	  => $ius_source_mirrorlist,
+      failovermethod      => $ius_source_failovermethod,
+      proxy               => $ius_source_proxy,
+      enabled             => $ius_source_enabled,
+      gpgcheck            => $ius_source_gpgcheck,
       gpgkey              => 'file:///etc/pki/rpm-gpg/IUS-COMMUNITY-GPG-KEY',
       descr               => "IUS Community Packages for Enterprise Linux ${::operatingsystemmajrelease} - \$basearch",
     }
 
     yumrepo { 'ius-archive':
       baseurl             => $ius_archive_baseurl,
+      mirrorlist	  => $ius_archive_mirrorlist,
       failovermethod      => $ius_archive_failovermethod,
       proxy               => $ius_archive_proxy,
       enabled             => $ius_archive_enabled,
@@ -133,6 +137,7 @@ class ius (
 
     yumrepo { 'ius-archive-debuginfo':
       baseurl             => $ius_archive_debuginfo_baseurl,
+      mirrorlist	  => $ius_archive_debuginfo_mirrorlist,
       failovermethod      => $ius_archive_debuginfo_failovermethod,
       proxy               => $ius_archive_debuginfo_proxy,
       enabled             => $ius_archive_debuginfo_enabled,
@@ -143,6 +148,7 @@ class ius (
 
     yumrepo { 'ius-archive-source':
       baseurl             => $ius_archive_source_baseurl,
+      mirrorlist	  => $ius_archive_source_mirrorlist,
       failovermethod      => $ius_archive_source_failovermethod,
       proxy               => $ius_archive_source_proxy,
       enabled             => $ius_archive_source_enabled,
@@ -153,6 +159,7 @@ class ius (
 
     yumrepo { 'ius-dev':
       baseurl             => $ius_dev_baseurl,
+      mirrorlist	  => $ius_dev_mirrorlist,
       failovermethod      => $ius_dev_failovermethod,
       proxy               => $ius_dev_proxy,
       enabled             => $ius_dev_enabled,
@@ -162,11 +169,12 @@ class ius (
     }
 
     yumrepo { 'ius-dev-debuginfo':
-      baseurl             => $ius_dev_debug_baseurl,
-      failovermethod      => $ius_dev_debug_failovermethod,
-      proxy               => $ius_dev_debug_proxy,
-      enabled             => $ius_dev_debug_enabled,
-      gpgcheck            => $ius_dev_debug_gpgcheck,
+      baseurl             => $ius_dev_debuginfo_baseurl,
+      mirrorlist	  => $ius_dev_debuginfo_mirrorlist
+      failovermethod      => $ius_dev_debuginfo_failovermethod,
+      proxy               => $ius_dev_debuginfo_proxy,
+      enabled             => $ius_dev_debuginfo_enabled,
+      gpgcheck            => $ius_dev_debuginfo_gpgcheck,
       gpgkey              => 'file:///etc/pki/rpm-gpg/IUS-COMMUNITY-GPG-KEY',
       descr               => "IUS Community Packages for Enterprise Linux ${::operatingsystemmajrelease} - \$basearch - Dev Debug",
     }
@@ -183,6 +191,7 @@ class ius (
 
     yumrepo { 'ius-testing':
       baseurl             => $ius_testing_baseurl,
+      mirrorlist	  => $ius_testing_mirrorlist,
       failovermethod      => $ius_testing_failovermethod,
       proxy               => $ius_testing_proxy,
       enabled             => $ius_testing_enabled,
@@ -192,17 +201,19 @@ class ius (
     }
 
     yumrepo { 'ius-testing-debuginfo':
-      baseurl             => $ius_testing_debug_baseurl,
-      failovermethod      => $ius_testing_debug_failovermethod,
-      proxy               => $ius_testing_debug_proxy,
-      enabled             => $ius_testing_debug_enabled,
-      gpgcheck            => $ius_testing_debug_gpgcheck,
+      baseurl             => $ius_testing_debuginfo_baseurl,
+      mirrorlist	  => $ius_testing_debuginfo_mirrorlist,
+      failovermethod      => $ius_testing_debuginfo_failovermethod,
+      proxy               => $ius_testing_debuginfo_proxy,
+      enabled             => $ius_testing_debuginfo_enabled,
+      gpgcheck            => $ius_testing_debuginfo_gpgcheck,
       gpgkey              => 'file:///etc/pki/rpm-gpg/IUS-COMMUNITY-GPG-KEY',
       descr               => "IUS Community Packages for Enterprise Linux ${::operatingsystemmajrelease} - \$basearch - Testing Debug",
     }
 
     yumrepo { 'ius-testing-source':
       baseurl             => $ius_testing_source_baseurl,
+      mirrorlist	  => $ius_testing_source_mirrorlist,
       failovermethod      => $ius_testing_source_failovermethod,
       proxy               => $ius_testing_source_proxy,
       enabled             => $ius_testing_source_enabled,
